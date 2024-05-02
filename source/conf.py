c342f4498b15939bb4913c448afad4e7118a6545
ping-armo-fati <115739749+ping-armo-fati@users.noreply.github.com>
2024-05-02 04:53:38 +0000

    init

---
+++ source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'My All Markdown Notes'
+copyright = '2024, Pingqiu Yuan'
+author = 'Pingqiu Yuan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+extensions = ['sphinx_rtd_theme']
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
---
+++ source/index.rst
@@ -0,0 +1,27 @@
+.. My All Markdown Notes documentation master file, created by
+   sphinx-quickstart on Tue Apr 30 12:43:09 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to My All Markdown by Pq Y
+=================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. warn::
+   这是一个警告
+
+.. info:: 
+   这是一个通知
+
+.. code-block:: bash
+   $ 这是一个shell
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
